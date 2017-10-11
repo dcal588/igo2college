9aabf1780c6a35056306b1b6f2261bcc8f0a1719
roy769 <roy@mortgallery.com>
2017-10-10 21:04:29 -0700

    hahah david

--- models/schoolData.js
+++ models/schoolData.js
@@ -1,12 +1,35 @@
  //salary
  module.exports = function(sequelize, DataTypes) {
  var Salary = sequelize.define("Salary", {
+   undergraduateMajor: DataTypes.STRING,
+   startingMedianSalary: DataTypes.STRING,
+   midCareerMedianSalary: DataTypes.STRING,
+   percentChangeStartToMidCareer: DataTypes.DECIMAL(5,1),
+   midCareer10thPercentile: DataTypes.STRING,
+   midCareer25thPercentile: DataTypes.STRING,
+   midCareer75thPercentile: DataTypes.STRING,
+   midCareer90thPercentile: DataTypes.STRING
+    });
 
-   })
+  var Degree = sequelize.define("Degree", {
+    region: DataTypes.STRING,
+    schoolName: DataTypes.STRING,
+    undergraduateMajor: DataTypes.STRING,
+    startingMedianSalary: DataTypes.STRING,
+    midCareerMedianSalary: DataTypes.STRING,
+    midCareer10thPercentile: DataTypes.STRING,
+    midCareer25thPercentile: DataTypes.STRING,
+    midCareer75thPercentile: DataTypes.STRING,
+    midCareer90thPercentile: DataTypes.STRING
+  });
+
+  var region
+  return Salary, Degree, Region;
+
  }
 
 
-//degree
+
 
 
 
