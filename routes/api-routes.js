83529dfbf2ae5d35049917523fd952baa03a4c9e
Erika <eyardumian@gmail.com>
2017-10-10 18:47:30 -0700

    fixed server

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,9 @@
 var db = require("../models");
 
-module.exports = function(app) {
-
-  
-}
+// module.exports = function(app) {
+//   app.get("/api/posts", function(req, res) {
+//     var query = {}// do i need this?
+//     if (req)
+//   })
+//
+// }
