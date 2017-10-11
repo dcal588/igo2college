84fbda1f693c85582f5406eff449a89e271f1424
Erika <eyardumian@gmail.com>
2017-10-10 21:01:59 -0700

    resolve

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,15 +1,15 @@
 var db = require("../models");
 
-module.exports = function(app) {
-
-  var queryScoreCard = "https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name&apikey=XdOHSc8fKhMKidPu2HWqCZmMy9OxtCJamGC580Bi";
-
-  app.get("/api/name", function(req, res) {
-  db.Name.findAll({}).then(function(dbName) {
-    res.json(dbName);
-    });
-  });
-};
+// module.exports = function(app) {
+//
+//   var queryScoreCard = "https://api.data.gov/ed/collegescorecard/v1/schools?fields=school.name&api_key=XdOHSc8fKhMKidPu2HWqCZmMy9OxtCJamGC580Bi";
+//
+//   app.get("/api/name", function(req, res) {
+//   db.Name.findAll({}).then(function(dbName) {
+//     res.json(dbName);
+//     });
+//   });
+// };
 
 //Did we compelete an activity in class using express and an API?
 //Do I use school, name, school.name, or school_name?
