0cfaa05d0bf98e1282ef26984df85f7ecf876eef
roy769 <roy@mortgallery.com>
2017-10-16 17:12:03 -0700

    'merging

--- controllers/sqlData_controller.js
+++ controllers/sqlData_controller.js
@@ -1,34 +1,44 @@
-//declare
-
-var express = require("express");
-
-var router = express.Router();
-
-var db = require("../models");
-
-//get route to index
-router.get('/', function(req,res) {
-  res.redirect("/schoolData");
-});
-
-//get college table
-router.get("/schoolData", function(req, res) {
-  db.IGO2COLLEGE.findAll({}).then(function(data) {
-    var hbsObject = { College: data };
-    res.render("index", hbsObject);
-  })
-});
-
-router.get("/schoolData", function(req, res) {
-  db.IGO2COLLEGE.findOne({
-    where: {
-      title: "schoolName",
-
-    }
-  }).then(function(data) {
-    var hbsObject = { College: data };
-    res.render("index.html", hbsObject);
-  })
-});
-console.log("suck it");
-//post to user api for now
+// //declare
+//
+// var express = require("express");
+//
+// var router = express.Router();
+//
+// var db = require("../models");
+//
+// //get route to index
+// router.get('/', function(req,res) {
+//   res.redirect("/schoolData");
+// });
+//
+// //get college table
+// router.get("/schoolData", function(req, res) {
+//   db.College.findAll({}).then(function(data) {
+//     var hbsObject = { schoolData: data };
+//     res.render("index", hbsObject);
+//   })
+// });
+//
+// router.get("/schoolData", function(req, res) {
+//   db.College.findOne({
+//     where: {
+//       title: "schoolName"
+//     }
+//   }).then(function(data) {
+//     var hbsObject = { college: data };
+//     res.render("index.html", hbsObject);
+//   })
+// });
+// //post to user
+// router.post("/schoolData/create", function (req,res) {
+//   db.Users.create({
+//     firstName:  req.body.firstName,
+//     lastName:   req.body.lastName,
+//     userName:   req.body.userName,
+//     password:   req.body.password
+//   }).then(function () {
+//     res.redirect("/");
+//   })
+// });
+//
+// module.export = router;
