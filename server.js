205a3aa58cf9305bab739a373744e0a4786310df
Erika <eyardumian@gmail.com>
2017-10-16 20:03:36 -0700

    login link working

--- server.js
+++ server.js
@@ -25,24 +25,6 @@ app.use(methodOverride('_method'))
 require("./routes/api-routes.js")(app);
 require("./routes/login-routes.js")(app);
 
-
-//login registration
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
-// var router = express.Router();
-// // test route
-// router.get('/', function(req, res) {
-//     res.json({ message: 'welcome to our upload module apis' });
-// });
-// //route to handle user registration
-// //router.post('/register',login.register);
-// router.post('/login',login.login)
-// app.use('/', router);
-//end login registration
-
 db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
