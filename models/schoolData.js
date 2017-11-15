6243cb69132277a9d2fce2393d9cd4634290b6bd
roy769 <roy@mortgallery.com>
2017-11-14 20:29:50 -0800

    es6 to schoolData

--- models/schoolData.js
+++ models/schoolData.js
@@ -1,6 +1,5 @@
-
-module.exports = function(sequelize, DataTypes) {
- var College = sequelize.define("College", {
+export default (sequelize, DataTypes) => {
+ const Colleges = sequelize.define("College", {
    schoolName: DataTypes.STRING,
    undergraduateMajor: DataTypes.STRING,
    startingMedianSalary: DataTypes.STRING,
@@ -9,31 +8,29 @@ module.exports = function(sequelize, DataTypes) {
    midCareer25thPercentileSalary: DataTypes.STRING,
    midCareer75thPercentileSalary: DataTypes.STRING,
    midCareer90thPercentileSalary: DataTypes.STRING
-    },{
-    timestamps: false
-});
+    });
 
-  // var Region = sequelize.define("Region", {
-  //   region: DataTypes.STRING,
-  //   schoolName: DataTypes.STRING,
-  //   undergraduateMajor: DataTypes.STRING,
-  //   startingMedianSalary: DataTypes.STRING,
-  //   midCareerMedianSalary: DataTypes.STRING,
-  //   midCareer10thPercentile: DataTypes.STRING,
-  //   midCareer25thPercentile: DataTypes.STRING,
-  //   midCareer75thPercentile: DataTypes.STRING,
-  //   midCareer90thPercentile: DataTypes.STRING
-  // });
+  const Regions = sequelize.define("Region", {
+    region: DataTypes.STRING,
+    schoolName: DataTypes.STRING,
+    undergraduateMajor: DataTypes.STRING,
+    startingMedianSalary: DataTypes.STRING,
+    midCareerMedianSalary: DataTypes.STRING,
+    midCareer10thPercentile: DataTypes.STRING,
+    midCareer25thPercentile: DataTypes.STRING,
+    midCareer75thPercentile: DataTypes.STRING,
+    midCareer90thPercentile: DataTypes.STRING
+  });
 
-  // var Degree = sequelize.define("Degree", {
-  //   undergraduateMajor: DataTypes.STRING,
-  //   startingMedianSalary: DataTypes.STRING,
-  //   midCareerMedianSalary: DataTypes.STRING,
-  //   percentchangefromStartToMidCareer: DataTypes.DECIMAL(5,1),
-  //   midCareer10thPercentile: DataTypes.STRING,
-  //   midCareer25thPercentile: DataTypes.STRING,
-  //   midCareer75thPercentile: DataTypes.STRING,
-  //   midCareer90thPercentile: DataTypes.STRING
-  // });
-  return College;
- }
+  const Degrees = sequelize.define("Degree", {
+    undergraduateMajor: DataTypes.STRING,
+    startingMedianSalary: DataTypes.STRING,
+    midCareerMedianSalary: DataTypes.STRING,
+    percentchangefromStartToMidCareer: DataTypes.DECIMAL(5,1),
+    midCareer10thPercentile: DataTypes.STRING,
+    midCareer25thPercentile: DataTypes.STRING,
+    midCareer75thPercentile: DataTypes.STRING,
+    midCareer90thPercentile: DataTypes.STRING
+  });
+  return Colleges, Regions, Degrees;
+ };
